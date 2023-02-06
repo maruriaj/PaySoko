84ebbe1c2ab15b24bc6fbe9c25fcef7c98ba9183
Maruria Joseph <josephm@paysokosystems.com>
2023-02-06 15:45:28 +0300

    Desktop and Recommended Changes

--- src/Requests.js
+++ src/Requests.js
@@ -1,10 +1,10 @@
 export const fetchCategories = () => {
   try {
-    return fetch('https://api.paysoko.shop/api/v1/admin/categories').then(
+    return fetch("https://api.paysoko.shop/api/v1/admin/categories").then(
       (response) => response.json()
     );
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
   }
 };
 
@@ -14,13 +14,13 @@ export const fetchProducts = (id) => {
       `https://api.paysoko.shop/api/v1/admin/products/category/${id}`
     ).then((response) => response.json());
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
   }
 };
 
 export const fetchSubCategories = (id) => {
   fetch(`http://api.paysoko.shop/api/v1/admin/sub-categories/${id}`, {
-    mode: 'no-cors',
+    mode: "no-cors",
   })
     .then((response) => response.json())
     .then((data) => {
@@ -28,6 +28,6 @@ export const fetchSubCategories = (id) => {
       return data.data;
     })
     .catch((error) => {
-      console.error('Error:', error);
+      console.error("Error:", error);
     });
 };
